9f4b8b622b88bc56214312cb8288bf28dfce4b16
QuanMa722 <quanma722@gmail.com>
2024-10-07 23:29:59 +0800

    1007

--- Three/Erich/code.py
+++ Three/Erich/code.py
@@ -7,7 +7,7 @@ from rich import box
 console = Console()
 
 users = [
-    {'id': '1', 'name': 'alice', 'age': '13'},
+    {'id': '1', 'name': 'alice', 'age': '16'},
     {'id': '2', 'name': 'bob', 'age': '15'},
     {'id': '3', 'name': 'kate', 'age': '19'},
 ]
